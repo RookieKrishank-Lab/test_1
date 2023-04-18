f9cb588addf007a9d79263c28094128f2432fc73
Krishank <sarmakrishank@gmail.com>
2023-04-18 12:44:51 +0530

    adding files

--- src/com/Interface/test1.java
+++ src/com/Interface/test1.java
@@ -9,13 +9,21 @@ class sec1 {}
 
 //class sec1 implements sec1{}                    //Not allowed
 
-interface a1{}
+interface a1{
+
+    public int method1();
+}
 
 //interface a1 implements sec1{}                //interface cant implement class
 
 //interface a1 extends sec1{}                   //interface cant extends class
 
-class a4 implements a1{}                        //class can implements interface
+class a4 implements a1{
+    @Override
+    public int method1() {
+        return 0;
+    }
+}                        //class can implements interface
 
 //class a5 extends a1{}                           //class cant extends interface
 
@@ -27,7 +35,7 @@ interface a2{
 
 //    void m3();          //by default if we dont define the access modifier for method it will be abstract
 
-    private void m4(){}
+//    private void m4(){}
 }
 
 interface B1 {
@@ -37,7 +45,12 @@ interface B1 {
     }
 }
 
-//interface A1 extends B1 {} // allowed
+interface A1 extends B1,a2 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}                   // allowed
 
-//interface A1 implements B1 {} // not allowed
+
+//interface A1 implements B1 {}                   // not allowed because interface might not provide implementation of abstract method
 
